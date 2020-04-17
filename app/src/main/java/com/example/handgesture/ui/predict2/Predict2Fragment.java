76add712c267d1fccfd822e6b93757eab6403117
tweetychain <59549819+tweetychain@users.noreply.github.com>
2020-04-17 14:49:25 +0700

    fragment_predict2

---
+++ app/src/main/java/com/example/handgesture/ui/predict2/Predict2Fragment.java
@@ -0,0 +1,38 @@
+package com.example.handgesture.ui.predict2;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.handgesture.R;
+
+public class Predict2Fragment extends Fragment {
+
+    private Predict2ViewModel mViewModel;
+
+    public static Predict2Fragment newInstance() {
+        return new Predict2Fragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_predict2, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(Predict2ViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/handgesture/ui/predict2/Predict2ViewModel.java
@@ -0,0 +1,7 @@
+package com.example.handgesture.ui.predict2;
+
+import androidx.lifecycle.ViewModel;
+
+public class Predict2ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
